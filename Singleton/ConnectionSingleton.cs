e47027b877a8517ecf5e44924f605c383316c65f
MRP <ipazooki@gmail.com>
2020-07-04 16:32:05 +0100

    Add Singleton

---
+++ Singleton/ConnectionSingleton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    /// <summary>
+    /// Singleton class
+    /// </summary>
+    public sealed class ConnectionSingleton
+    {
+        /// <summary>
+        /// Static members will be loaded eagerly immediately when class is loaded.
+        /// </summary>
+        private static readonly ConnectionSingleton instance = new ConnectionSingleton();
+
+        private readonly List<string> connectionStrings;
+
+        private ConnectionSingleton()
+        {
+            connectionStrings = new List<string>() {
+                "My SQL connection string",
+                "SQL connection string"
+                };
+        }
+
+        public static ConnectionSingleton GetInstance()
+        {
+            return instance;
+        }
+
+        public List<string> GetConnectionStrings()
+        {
+            return connectionStrings;
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Singleton
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var one = ConnectionSingleton.GetInstance();
+
+            var two = ConnectionSingleton.GetInstance();
+
+            if (one == two)
+            {
+                Console.WriteLine("They are same instances");
+            }
+
+            foreach (var item in one.GetConnectionStrings())
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
